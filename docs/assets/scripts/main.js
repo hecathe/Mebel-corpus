b7c00b8c99609f3a452f637efeb7fae5ea7e3c43
Kate <catherina1punk@gmail.com>
2021-10-03 00:33:09 +0300

    build main page

--- docs/assets/scripts/main.js
+++ docs/assets/scripts/main.js
@@ -36,7 +36,8 @@ if (tabIdList) {
 	}
 }
 
-const swiper = new Swiper('[data-swiper="one-slide"]', {
+//sliders
+const swiper = new Swiper('.swiper-oneslide', {
 	// Optional parameters
 	loop: true,
 	speed: 500,
@@ -53,52 +54,91 @@ const swiper = new Swiper('[data-swiper="one-slide"]', {
 	},
 })
 
+const cardsSwiper = new Swiper('.cards__slider .swiper-fourslide', {
+	// Optional parameters
+	loop: false,
+	speed: 500,
+	slidesPerView: 1,
+	spaceBetween: 20,
+
+	breakpoints: {
+		500: {
+			slidesPerView: 2,
+		},
+		700: {
+			slidesPerView: 3,
+		},
+		1024: {
+			slidesPerView: 4,
+		}
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.cards__slider .swiper-button-next',
+		prevEl: '.cards__slider .swiper-button-prev',
+	},
+})
+
+const swiperWithoutPag = new Swiper('.sales__slider .swiper-container', {
+	// Optional parameters
+	loop: true,
+	speed: 500,
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.sales__slider .swiper-button-next',
+		prevEl: '.sales__slider .swiper-button-prev',
+	},
+})
+
 //header
-
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-// function toggle() {
-// 	document.querySelector('[data-full-menu]').classList.toggle("show");
-// }
-  
-// // Close the dropdown menu if the user clicks outside of it
-// window.onclick = function(event) {
-// 	if (!event.target.matches('.dropbtn')) {
-// 	  	var dropdowns = document.getElementsByClassName("dropdown-content");
-// 	  	var i;
-// 	  	for (i = 0; i < dropdowns.length; i++) {
-// 			var openDropdown = dropdowns[i];
-// 			if (openDropdown.classList.contains('show')) {
-// 		  		openDropdown.classList.remove('show');
-// 			}
-// 	  	}
-// 	}
-// }
-
-const toggleFullMenu = document.querySelector('[data-catalog-toggle]');
+const toggleFullMenu = document.querySelectorAll('[data-catalog-toggle]');
 const closeBtn = document.querySelector('[data-close]');
+const searchBtn = document.querySelector('.search-form__btn');
+const burgerBtn = document.querySelector('[data-burger]')
+const moreBtn = document.querySelector('[data-more-text]');
 
 window.onclick = (event) => {
 	const fullMenu = document.querySelector('[data-full-menu');
 
-	if (event.target == toggleFullMenu) {
-		fullMenu.classList.add('show')
-	}
+	toggleFullMenu.forEach((item) => {
+		if (event.target == item) {
+			fullMenu.classList.add('show')
+		}
+	})
 
 	if (event.target == closeBtn) {
 		fullMenu.classList.remove('show')
 	}
+
+	if (event.target == searchBtn) {
+		searchBtn.nextElementSibling.classList.add('show')
+	} else if (event.target !== searchBtn.nextElementSibling.childNodes[1]) {
+		searchBtn.nextElementSibling.classList.remove('show')
+	}
+
+	if (event.target == burgerBtn) {
+		const header = document.querySelector('.header')
+		const body = document.body;
+
+		header.classList.toggle('active')
+
+		if (header.classList.contains('active')) {
+			body.style.overflow = 'hidden'
+		} else {
+			body.removeAttribute('style')
+		}
+	}
+
+	if (event.target == moreBtn) {
+		moreBtn.previousElementSibling.classList.toggle('isOpen');
+
+		if (moreBtn.previousElementSibling.classList.contains('isOpen')) {
+			moreBtn.innerHTML = 'Свернуть'
+		} else {
+			moreBtn.innerHTML = 'Развернуть'
+		}
+	}
 }
 
-
-
-
-// /* Open */
-// function openNav() {
-// 	document.getElementById("myNav").style.height = "100%";
-//   }
-  
-//   /* Close */
-//   function closeNav() {
-// 	document.getElementById("myNav").style.height = "0%";
-//   }
