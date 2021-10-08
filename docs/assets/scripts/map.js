552cad02cfe61e5c6986d198f53ce8f637550c78
Екатерина Филиппова <catherina1punk@gmail.com>
2021-10-08 18:00:09 +0300

    + prod what in progress

---
+++ docs/assets/scripts/map.js
@@ -0,0 +1,38 @@
+ymaps.ready(init);
+
+function init () {
+    var myMap = new ymaps.Map('map', {
+        center: [59.808459, 30.459113],
+        zoom: 9,
+        controls: []
+    });
+
+    myGeoObject = new ymaps.GeoObject({
+        // Описание геометрии.
+        geometry: {
+            type: "Point",
+            coordinates: [59.808459, 30.459113],
+        },
+    });
+    myMap.geoObjects.add(myGeoObject)
+
+    const select = document.querySelector('.contacts__select select');
+
+    select.addEventListener('change', () => {
+        const coords = select.value.split(',')
+        myMap.setCenter(coords)
+
+        myGeoObject = new ymaps.GeoObject({
+            // Описание геометрии.
+            geometry: {
+                type: "Point",
+                coordinates: coords,
+            },
+        });
+        myMap.geoObjects.add(myGeoObject)
+    })
+
+    
+    
+}
+
